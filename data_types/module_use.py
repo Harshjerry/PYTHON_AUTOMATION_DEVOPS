8dd901cb5cb88991ff69f8b18ef54f336b4d3824
harsh <harshdeep1043@gmail.com>
2026-03-06 16:04:14 +0530

    Secrets adn command line  args

--- data_types/module_use.py
+++ data_types/module_use.py
@@ -1,4 +1,18 @@
 import modules_defined as m 
+import sys
+import os
+# num1=sys.argv[1]     wont work as it will be string and we need to convert it to int or float
+# operation=sys.argv[2]
+# num2=sys.argv[3]
 
-sum=m.add(5, 10)    # we are using   add function using  module approach lllll
-print(sum)
+num1=float(sys.argv[1])
+operation=sys.argv[2]
+num2=float(sys.argv[3])
+
+
+if operation == "add":
+    result = m.add(num1, num2)
+    print(result)
+
+#similary for  sensitibe  informaitonm we use env  variables
+os.getenv("SECRET")
